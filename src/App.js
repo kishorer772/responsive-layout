7143bf0823ff4be7196882c444f57e708a937825
kishore puthalapattu <kishore.puthalapattu@gmail.com>
2024-02-08 17:48:09 +0530

    Added Framer motion for styling ease for post items

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import './assets/css/styles.css';
 import Comments from './Comments';
 import Footer from './Footer';
 import consoleLogger from './hooks/logger';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
   console.log(process.env.REACT_APP_ENV);
   consoleLogger('app');
   return (
     <div>
-      <div className="h-full overflow-hidden">
-        <p className="bg-red-500 text-center text-white text-2xl p-5 lg:text-left md:text-xl  w-screen ">
-          React Resonsive form
-        </p>
-        <Comments />
-        <Footer />
-      </div>
+      <AnimatePresence>
+        <div className="h-full overflow-hidden">
+          <motion.p className="bg-red-500 text-center text-white text-2xl p-5 lg:text-left md:text-xl  w-screen ">
+            React Resonsive form
+          </motion.p>
+        </div>
+      </AnimatePresence>
+      <Comments />
+      <Footer />
     </div>
   );
 }
--- src/PostItem.jsx
+++ src/PostItem.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
-
+import { AnimatePresence, motion } from 'framer-motion';
 const PostItem = ({ post }) => {
   return (
-    <div className="flex-col p-1  h-full xs:flex-1  w-full md:w-1/2 lg:w-1/3 xl:w-1/3">
-      <div className="bg-lime-600 p-5 rounded-xl">
-        <div className="m-2 h-60 p-2 shrink mt-1">
-          <p className="text-xl p-4 text-white text-ellipsis overscroll-hidden  xs:text-center  ">
-            {post.title}
-          </p>
-          <p className="p-2  w-52 text-sm text-ellipsis  overflow-hidden h-min ">
-            {post.body}
-          </p>
+    <AnimatePresence>
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        className="flex-col p-1  h-full xs:flex-1  w-full md:w-1/2 lg:w-1/3 xl:w-1/3"
+      >
+        <div className="bg-lime-600 p-5 rounded-xl">
+          <div className="m-2 h-60 p-2 shrink mt-1">
+            <p className="text-xl p-4 text-white text-ellipsis overscroll-hidden  xs:text-center  ">
+              {post.title}
+            </p>
+            <p className="p-2  w-52 text-sm text-ellipsis  overflow-hidden h-min ">
+              {post.body}
+            </p>
+          </div>
+          <div className="flex justify-center  xs:flex-row sm:flex-row md:flex-col xl:flex-row ">
+            <motion.span
+              whileTap={{ opacity: 0.8, color: 'white' }}
+              className=" bg-blue-500 p-2  cursor-pointer   text-center xs:mr-1 sm:mr-1  md:mb-1  xl:mr-1   "
+            >
+              Comment
+            </motion.span>
+            <motion.span
+              whileTap={{ opacity: 0.8, color: 'white' }}
+              className=" bg-red-500  p-2  cursor-pointer   text-center xs:mr-1  sm:mr-1 md:mb-1  xl:mr-1  "
+            >
+              Comment
+            </motion.span>
+            <motion.span
+              whileTap={{ opacity: 0.8, color: 'white' }}
+              className=" bg-fuchsia-500 p-2 cursor-pointer   text-center"
+            >
+              Comment
+            </motion.span>
+          </div>
         </div>
-        <div className="flex justify-center  xs:flex-row sm:flex-row md:flex-col xl:flex-row ">
-          <span className=" bg-blue-500 p-2  cursor-pointer   text-center xs:mr-1 sm:mr-1  md:mb-1  xl:mr-1   ">
-            Comment
-          </span>
-          <span className=" bg-red-500  p-2  cursor-pointer   text-center xs:mr-1  sm:mr-1 md:mb-1  xl:mr-1  ">
-            Comment
-          </span>
-          <span className=" bg-fuchsia-500 p-2 cursor-pointer   text-center">
-            Comment
-          </span>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
